12ee2d89ceb71d542e9a630dc9dd696b987074cf
ValeroMateo <valero.mateo98@gmail.com>
2020-02-13 22:20:16 +0100

    wtf

---
+++ src/main/java/es/salesianos/model/PokeballSuperball.java
@@ -0,0 +1,12 @@
+package es.salesianos.model;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component("pokeball")
+@Profile("pokeballSuperball")
+public class PokeballSuperball extends AbstractPokeball {
+	public PokeballSuperball() {
+		setCatchChancePercentage(50);
+	}
+}
---
+++ src/main/java/es/salesianos/model/PokeballUltraball.java
@@ -0,0 +1,13 @@
+package es.salesianos.model;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component("pokeball")
+@Profile("pokeballUltraball")
+public class PokeballUltraball extends AbstractPokeball {
+	public PokeballUltraball() {
+		setCatchChancePercentage(75);
+	}
+}
+
